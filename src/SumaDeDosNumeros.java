63edf3ee2f8aa9c494c37a9e1b278468475ffc0d
theoto23 <otonielmonegro@gmail.com>
2022-01-22 14:13:49 -0400

    Aprendiendo

---
+++ src/SumaDeDosNumeros.java
@@ -0,0 +1,10 @@
+public class SumaDeDosNumeros {
+    public static void main(String[] args) {
+        int a = 5 ;
+        int b = 3;
+
+        int suma = a + b;
+
+        System.out.println( "La suma de " + a + " más " + b + " es " + suma + "." );
+    }
+}
